f23554caeb0cbd7f4d17524c58dca60547cf4d21
= <=>
2024-02-14 11:09:10 -0500

    Significant refactor; Added distance filter

--- skywatch.py
+++ skywatch.py
@@ -4,6 +4,8 @@ import time
 import csv
 import os
 
+from math import radians, sin, cos, sqrt, atan2
+
 # Add or remove these as needed
 SQUAWK_MEANINGS = {
     "7500": "Aircraft Hijacking",
@@ -18,6 +20,7 @@ SQUAWK_MEANINGS = {
     "1277": "Search & Rescue"
 }
 
+
 def load_env_file(file_path=".env"):
     with open(file_path) as file:
         for line in file:
@@ -26,15 +29,22 @@ def load_env_file(file_path=".env"):
                 key, value = line.strip().split("=", 1)
                 os.environ[key] = value
 
+
 load_env_file()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 WATCHLIST_FILE = os.getenv("WATCHLIST_FILE")
 AIRCRAFT_JSON_URI = os.getenv("AIRCRAFT_JSON_URI")
-AIRCRAFT_CEILING = os.getenv("AIRCRAFT_CEILING")
-ALTITUDE_FILTER = os.getenv("ALTITUDE_FILTER")
+AIRCRAFT_CEILING = int(os.getenv("AIRCRAFT_CEILING"))
+ALTITUDE_FILTER = int(os.getenv("ALTITUDE_FILTER"))
 SCRIPT_INTERVAL = os.getenv("SCRIPT_INTERVAL")
+DISTANCE_FILTER = int(os.getenv("DISTANCE_FILTER"))
+DISTANCE_ALERT = float(os.getenv("DISTANCE_ALERT"))
+HOME_LAT = float(os.getenv("HOME_LAT"))
+HOME_LON = float(os.getenv("HOME_LON"))
+LOGGING = int(os.getenv("LOGGING"))
+
 
 def load_watchlist():
     watchlist = {}
@@ -75,10 +85,67 @@ def load_csv_data(filename):
     return csv_data
 
 
+def haversine(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth specified in decimal degrees
+    """
+    # Convert decimal degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    # Radius of the Earth in miles
+    R = 3958.8  # miles
+    distance = R * c
+    print(f'Distance: {distance}') if LOGGING == 1 else None
+
+    if (distance <= DISTANCE_ALERT):
+        return True
+
+    return False
+
+
+def check_altitude(baro, geom):
+    if ALTITUDE_FILTER == 1:
+        if int(baro) == 999999 and int(geom) == 999999:
+            print('Altitude not recorded yet') if LOGGING == 1 else None
+            return False
+        if int(baro) <= AIRCRAFT_CEILING or int(geom) <= AIRCRAFT_CEILING:
+            print('Altitude below alert ceiling') if LOGGING == 1 else None
+            return True
+        else:
+            print('Aircraft above alert altitude') if LOGGING == 1 else None
+            return False
+    else:
+        print('Altitude filter disabled') if LOGGING == 1 else None
+        return True
+
+
+def check_distance(lat, lon):
+    if DISTANCE_FILTER == 1:
+        if lat == -1 or lon == -1:
+            print("Lat or Lon -1") if LOGGING == 1 else None
+            return False
+        if haversine(lat, lon, HOME_LAT, HOME_LON):
+            print("Aircraft inside fence") if LOGGING == 1 else None
+            return True
+        else:
+            print("Aircraft outside of fence") if LOGGING == 1 else None
+            return False
+    else:
+        print("Distance filter disabled") if LOGGING == 1 else None
+        return True
+
+
 def main():
     squawk_alert_history = {}
     watchlist_alert_history = {}
-    csv_files = ["plane-alert-civ-images.csv", "plane-alert-mil-images.csv", "plane-alert-gov-images.csv"]
+    csv_files = ["plane-alert-civ-images.csv",
+                 "plane-alert-mil-images.csv", "plane-alert-gov-images.csv"]
     csv_data = {}
 
     for filename in csv_files:
@@ -93,8 +160,10 @@ def main():
             hex_code = aircraft['hex'].upper()
             flight = aircraft.get('flight', '').strip().upper()
             squawk = aircraft.get('squawk', '')
-            altitude_geom = aircraft.get('alt_geom',0)
-            altitude_baro = aircraft.get('alt_baro',0)
+            altitude_geom = aircraft.get('alt_geom', 999999)
+            altitude_baro = aircraft.get('alt_baro', 999999)
+            acft_lat = float(aircraft.get("lat", -1))
+            acft_lon = float(aircraft.get("lon", -1))
 
             # Alert on specific squawk codes
             if squawk in SQUAWK_MEANINGS and (
@@ -122,21 +191,22 @@ def main():
                 if status_code == 200:
                     message_lines = message.split('\n')[:3]
                     for line in message_lines:
-                        print(line)
+                        print(line) if LOGGING == 1 else None
                 else:
-                    print(f"Failed to send squawk alert. Status Code: {status_code}")
+                    print(
+                        f"Failed to send squawk alert. Status Code: {status_code}")
 
             # Alert on items in the watchlist
             for entry in watchlist:
-                if (not ALTITUDE_FILTER or 
-                    (ALTITUDE_FILTER and (int(altitude_geom != 0) and int(altitude_baro != 0)) and 
-                     int(altitude_geom) <= int(AIRCRAFT_CEILING) and 
-                     int(altitude_baro) <= int(AIRCRAFT_CEILING))):
-                    if entry.endswith('*'):
-                        if fnmatch.fnmatch(flight, entry):
+                if entry.endswith('*'):
+                    if fnmatch.fnmatch(flight, entry):
+                        print(
+                            f'Matched Hex: {hex_code} Label: {watchlist[entry]}') if LOGGING == 1 else None
+                        if check_altitude(altitude_baro, altitude_geom) and check_distance(acft_lat, acft_lon):
                             if (hex_code not in watchlist_alert_history or
                                     time.time() - watchlist_alert_history[hex_code] >= 3600):
-                                watchlist_alert_history[hex_code] = time.time()
+                                watchlist_alert_history[hex_code] = time.time(
+                                )
                                 if hex_code in csv_data:
                                     context = csv_data[hex_code]
                                     message = (
@@ -167,12 +237,17 @@ def main():
                                 if status_code == 200:
                                     message_lines = message.split('\n')[:3]
                                     for line in message_lines:
-                                        print(line)
+                                        print(line) if LOGGING == 1 else None
                                 else:
-                                    print(f"Failed to send watchlist alert. Status Code: {status_code}")
-                    elif hex_code == entry or flight == entry:
+                                    print(
+                                        f"Failed to send watchlist alert. Status Code: {status_code}")
+                elif hex_code == entry or flight == entry:
+                    print(
+                        f'Matched Hex: {hex_code} Label: {watchlist[entry]}') if LOGGING == 1 else None
+                    if check_altitude(altitude_baro, altitude_geom) and check_distance(acft_lat, acft_lon):
                         if hex_code not in watchlist_alert_history or time.time() - watchlist_alert_history[hex_code] >= 3600:
-                            watchlist_alert_history[hex_code] = time.time()
+                            watchlist_alert_history[hex_code] = time.time(
+                            )
                             if hex_code in csv_data:
                                 context = csv_data[hex_code]
                                 message = (
@@ -203,9 +278,10 @@ def main():
                             if status_code == 200:
                                 message_lines = message.split('\n')[:3]
                                 for line in message_lines:
-                                    print(line)
+                                    print(line) if LOGGING == 1 else None
                             else:
-                                print(f"Failed to send watchlist alert. Status Code: {status_code}")
+                                print(
+                                    f"Failed to send watchlist alert. Status Code: {status_code}")
 
         time.sleep(int(SCRIPT_INTERVAL))
 
